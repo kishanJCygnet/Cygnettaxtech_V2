9dfc763c66a2c406c7e24a2b22d462dd526ad260
Kishan <kishan.jaiswal@cygnetinfotech.com>
2023-04-17 18:25:58 +0530

    Sticky section create

--- wp-content/themes/cygnettaxtech/header.php
+++ wp-content/themes/cygnettaxtech/header.php
@@ -47,11 +47,11 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-KHZJ6C2');</script>
+	})(window,document,'script','dataLayer','GTM-KHZJ6C2');</script>-->
 	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
@@ -61,8 +61,8 @@ if(get_field('landing_page')[0] == 'Yes'){$landingpageclass = 'landing-page-cls'
 ?>
 <body <?php body_class($landingpageclass); ?>>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHZJ6C2"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHZJ6C2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
 <!-- End Google Tag Manager (noscript) -->
 <div class="loader">
     <img src="<?php echo THEME_PATH; ?>/images/Web loader Cygnature.gif" alt="loader" />
